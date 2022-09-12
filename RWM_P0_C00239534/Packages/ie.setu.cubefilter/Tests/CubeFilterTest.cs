6952cc5c6fdf6edc203a48ae18fb370e2e22fdf1
unknown <c0023934@itcarlow.ie>
2022-09-12 13:25:38 +0100

    real world sim 1 filrerst

---
+++ RWM_P0_C00239534/Packages/ie.setu.cubefilter/Tests/CubeFilterTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 10, 9, 12, 5, 5, 8 };
+            int[] output = CubeFilter.Module3(input);
+            int[] expected = { 1, 0, 0, 2, 2, 2 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+
+
+
+
+}
